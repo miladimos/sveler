a11b7c279cb5acb379a192b993d3881f88910f66
miladimos <milad.jafari6210@gmail.com>
2023-06-01 17:14:19 +0330

    add basic badge , button component

--- src/lib/components/buttons/Button.svelte
+++ src/lib/components/buttons/Button.svelte
@@ -1,26 +1,22 @@
 <script lang="ts">
 	import type { Colors } from '../types.js';
-	import type { ButtonType , ButtonStatus} from './button.types.js';
+	import type { ButtonType, ButtonStatus } from './button.types.js';
 
 	export let type: ButtonType = 'default';
 	export let status: ButtonStatus = 'normal';
-    export let color: Colors = 'primary';
-	export let href: string = null;
-    export let disabled: boolean = false;
-
+	export let color: Colors = 'danger';
+	export let href: string | null = null;
 </script>
 
-{#if href !== null}
-
-{:else}
-
-{/if}
+{#if href !== null}{:else}{/if}
 
 {#if type === 'default'}
 	<!-- {:else if} -->
 {/if}
 <!-- <a href="#" class="btn btn-primary w-100"> Primary </a> -->
 
-<button class="btn btn-{color}" on:click>
-    <slot />
+<button class="btn btn-{color}" on:click >
+	<slot />
 </button>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script>
-	import Badge from "$lib/components/badge/Badge.svelte";
-import Button from "$lib/components/buttons/Button.svelte";
-
+	import Badge from '$lib/components/badge/Badge.svelte';
+	import Button from '$lib/components/buttons/Button.svelte';
 </script>
 
-<h1>Welcome to your library project</h1>
+<Button >Button</Button>
 
-<Button>Button</Button>
-
+<br />
 <br>
 
-<Badge type="default" color="primary">Badge</Badge>
+<Badge type="default" color="danger">Badge</Badge>
+<Badge type="light" color="danger">Badge</Badge>
+<Badge type="outline" color="danger">Badge</Badge>
